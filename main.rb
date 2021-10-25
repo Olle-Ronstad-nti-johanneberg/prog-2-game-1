e41be5aeee6259a532fea96e0dcb0ca7cb9726c0
Olle Ronstad <olle.ronstad@gmail.com>
2021-10-25 12:44:24 +0200

    remove debug funtions etc

--- main.rb
+++ main.rb
@@ -6,9 +6,8 @@ require_relative 'Rockscater.rb'
 class Main < Gosu::Window
     def initialize
         super(640, 480)
-        @ground = Ground.new(self,"Ground data/debug.csv",Gosu::Color.argb(0xff_ffffff),Gosu::Color.argb(0xff_000000))
-        @rock = Rock.new(self,640/2,200,2,0.1,'nedladdning.jpg')
-        @rockscater = Rockscater.new(self,@ground,100,@rock)
+        @ground = Ground.new(self,"Ground data/ground1.csv",Gosu::Color.argb(0xff_ffffff),Gosu::Color.argb(0xff_000000))
+        @rockscater = Rockscater.new(self,@ground,10,1,0.1,'nedladdning.jpg')
     end
 
     def draw
