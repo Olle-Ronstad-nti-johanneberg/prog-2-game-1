f201d16c664cdca04e13fbe57d1e589848920b5d
Olle Ronstad <olle.ronstad@gmail.com>
2021-11-08 09:52:07 +0100

    change main to use level class

--- main.rb
+++ main.rb
@@ -1,18 +1,15 @@
 require 'Gosu'
-require_relative 'Ground.rb'
-require_relative 'Rockscater.rb'
 
+require_relative 'Level.rb'
 
 class Main < Gosu::Window
     def initialize
         super(640, 480)
-        @ground = Ground.new(self,"Ground data/ground1.csv",Gosu::Color.argb(0xff_ffffff),Gosu::Color.argb(0xff_000000))
-        @rockscater = Rockscater.new(self,@ground,20,1,0.1,'nedladdning.jpg')
+        @level = Level.new(self,'Level data/testLevel.csv')
     end
 
     def draw
-        @ground.draw
-        @rockscater.draw
+        @level.draw
     end
 end
 
