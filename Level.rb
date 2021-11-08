565ec38b809a36ef6a1d96abdcac7ed65665456b
Olle Ronstad <olle.ronstad@gmail.com>
2021-11-08 09:51:24 +0100

    add level calss to contain info about curent level

---
+++ Level.rb
@@ -0,0 +1,26 @@
+require 'csv'
+
+require_relative 'HEXColor.rb'
+require_relative 'Ground.rb'
+require_relative 'Rockscater'
+
+class Level
+    def initialize(window,levelpath)
+        @window = window
+        @data = {}
+        CSV.foreach(levelpath) do |row|
+            if row[1].to_f.to_s == row[1]
+                @data[row[0].to_sym] = row[1].to_f
+            else
+                @data[row[0].to_sym] = row[1]
+            end
+        end
+        @ground = Ground.new(@window,@data[:ground],Hex(@data[:topColor]),Hex(@data[:botomColor]))
+        @rockscater = Rockscater.new(@window,@ground,20,1,0.1,@data[:rockIMG])
+    end
+
+    def draw()
+        @ground.draw
+        @rockscater.draw
+    end
+end
