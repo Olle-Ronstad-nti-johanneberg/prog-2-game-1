f7c461d03de44842793f8a073aae61fce1dfa96b
Olle Ronstad <olle.ronstad@gmail.com>
2021-11-15 09:22:21 +0100

    change, center the text

--- startmenu.rb
+++ startmenu.rb
@@ -4,13 +4,13 @@ class StartMenu
     def initialize(window)
         @window = window
         @levels = Dir.children('./Level Data').map do |name|
-            Gosu::Image.from_text(name,20)
+            Gosu::Image.from_text(name.gsub(".csv",""),20, {bold: true})
         end
     end
 
     def draw
         @levels.each_with_index do |level, i|
-            level.draw(0,i*25)
+            level.draw((@window.width-level.width)*0.5,i*25)
         end
     end
 end
