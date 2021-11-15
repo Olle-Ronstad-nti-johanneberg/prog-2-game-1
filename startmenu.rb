32203fe1906f0ccf1f0fb3759429b23c2ff54ed3
Olle Ronstad <olle.ronstad@gmail.com>
2021-11-15 09:35:38 +0100

    add logo text thingy

--- startmenu.rb
+++ startmenu.rb
@@ -6,11 +6,13 @@ class StartMenu
         @levels = Dir.children('./Level Data').map do |name|
             Gosu::Image.from_text(name.gsub(".csv",""),20, {bold: true})
         end
+        @maintext = Gosu::Image.from_text("LUNAR GAME",75, {bold: true, font: "impact"})
     end
 
     def draw
+        @maintext.draw((@window.width-@maintext.width)*0.5,25)
         @levels.each_with_index do |level, i|
-            level.draw((@window.width-level.width)*0.5,i*25)
+            level.draw((@window.width-level.width)*0.5,i*25+125)
         end
     end
 end
