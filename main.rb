49dc860dddce7a3c97ee8eaee1370434b7cdb4d9
Olle Ronstad <olle.ronstad@gmail.com>
2021-11-15 09:57:03 +0100

    add states for menu and level

--- main.rb
+++ main.rb
@@ -1,15 +1,23 @@
 require 'Gosu'
 
+require_relative 'startmenu'
 require_relative 'Level.rb'
 
 class Main < Gosu::Window
     def initialize
         super(640, 480)
         @level = Level.new(self,'Level data/testLevel.csv')
+        @startmenu = StartMenu.new(self)
+        @state = "startMenu"
     end
 
     def draw
-        @level.draw
+        case @state
+        when "startMenu"
+            @startmenu.draw
+        when "level"
+            @level.draw
+        end
     end
 end
 
