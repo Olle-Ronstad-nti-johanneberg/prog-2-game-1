aeb0e65141b684756ea810f3da42c479b2e1ab7e
Olle Ronstad <olle.ronstad@gmail.com>
2021-11-22 09:26:53 +0100

    change hardcoded values to constants variables

--- startmenu.rb
+++ startmenu.rb
@@ -1,29 +1,31 @@
 require 'Gosu'
 
-GRILLKORV = 75
+MAINFONTSIZE = 75
+LEVELFONTSIZE = 20
+LEVELFONTTEXTSPACING = 5
 
 class StartMenu
     def initialize(window)
         @window = window
         @levelsPath = Dir.children('./Level Data')
         @levels = Dir.children('./Level Data').map do |name|
-            Gosu::Image.from_text(name.gsub(".csv",""),20, {bold: true})
+            Gosu::Image.from_text(name.gsub(".csv",""),LEVELFONTSIZE, {bold: true})
         end
-        @maintext = Gosu::Image.from_text("LUNAR GAME",GRILLKORV, {bold: true, font: "impact"})
-        @leftArrow = Gosu::Image.from_text("<",20, {bold: true, font: "impact"})
-        @rightArrow = Gosu::Image.from_text(">",20, {bold: true, font: "impact"})
+        @maintext = Gosu::Image.from_text("LUNAR GAME",MAINFONTSIZE, {bold: true, font: "impact"})
+        @leftArrow = Gosu::Image.from_text("<",LEVELFONTSIZE, {bold: true, font: "impact"})
+        @rightArrow = Gosu::Image.from_text(">",LEVELFONTSIZE, {bold: true, font: "impact"})
 
     end
 
     def draw
-        @maintext.draw((@window.width-@maintext.width)*0.5,25)
+        @maintext.draw((@window.width-@maintext.width)*0.5,LEVELFONTSIZE+LEVELFONTTEXTSPACING)
         @levels.each_with_index do |level, i|
-            level.draw((@window.width-level.width)*0.5,i*25+125)
+            level.draw((@window.width-level.width)*0.5,i*(LEVELFONTSIZE+LEVELFONTTEXTSPACING)+MAINFONTSIZE*1.5)
         end
         hoveringID = idHoveringOver()
         if !hoveringID.nil?
-            @leftArrow.draw((@window.width+@levels[hoveringID].width)*0.5,hoveringID*25+125)
-            @rightArrow.draw((@window.width-@levels[hoveringID].width)*0.5-@rightArrow.width,hoveringID*25+125)
+            @leftArrow.draw((@window.width+@levels[hoveringID].width)*0.5,hoveringID*(LEVELFONTSIZE+LEVELFONTTEXTSPACING)+MAINFONTSIZE*1.5)
+            @rightArrow.draw((@window.width-@levels[hoveringID].width)*0.5-@rightArrow.width,hoveringID*(LEVELFONTSIZE+LEVELFONTTEXTSPACING)+MAINFONTSIZE*1.5)
         end
 
     end
@@ -41,7 +43,7 @@ class StartMenu
 
     def idHoveringOver
         if @window.mouse_x > @window.width/4 && @window.mouse_x < @window.width/4*3
-            tmp = ((@window.mouse_y-125)/25).floor
+            tmp = ((@window.mouse_y-MAINFONTSIZE*1.5)/(LEVELFONTSIZE+LEVELFONTTEXTSPACING)).floor
             if tmp < 0 || tmp > @levelsPath.length-1
                 return nil
             else
