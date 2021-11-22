ceb84566a9318baad23b479637475a9c42bb1a8d
Olle Ronstad <olle.ronstad@gmail.com>
2021-11-22 09:18:41 +0100

    add arrows to indecate which level mouse is over

--- startmenu.rb
+++ startmenu.rb
@@ -1,5 +1,7 @@
 require 'Gosu'
 
+GRILLKORV = 75
+
 class StartMenu
     def initialize(window)
         @window = window
@@ -7,7 +9,10 @@ class StartMenu
         @levels = Dir.children('./Level Data').map do |name|
             Gosu::Image.from_text(name.gsub(".csv",""),20, {bold: true})
         end
-        @maintext = Gosu::Image.from_text("LUNAR GAME",75, {bold: true, font: "impact"})
+        @maintext = Gosu::Image.from_text("LUNAR GAME",GRILLKORV, {bold: true, font: "impact"})
+        @leftArrow = Gosu::Image.from_text("<",20, {bold: true, font: "impact"})
+        @rightArrow = Gosu::Image.from_text(">",20, {bold: true, font: "impact"})
+
     end
 
     def draw
@@ -15,6 +20,12 @@ class StartMenu
         @levels.each_with_index do |level, i|
             level.draw((@window.width-level.width)*0.5,i*25+125)
         end
+        hoveringID = idHoveringOver()
+        if !hoveringID.nil?
+            @leftArrow.draw((@window.width+@levels[hoveringID].width)*0.5,hoveringID*25+125)
+            @rightArrow.draw((@window.width-@levels[hoveringID].width)*0.5-@rightArrow.width,hoveringID*25+125)
+        end
+
     end
 
     def hoveringOverPath
@@ -31,9 +42,8 @@ class StartMenu
     def idHoveringOver
         if @window.mouse_x > @window.width/4 && @window.mouse_x < @window.width/4*3
             tmp = ((@window.mouse_y-125)/25).floor
-            if tmp < 0
+            if tmp < 0 || tmp > @levelsPath.length-1
                 return nil
-            elsif tmp > @levelsPath.length-1
             else
                 return tmp
             end
