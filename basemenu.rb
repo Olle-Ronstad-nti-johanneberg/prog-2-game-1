200e86856d3cc9488dee43c4c9ac61526e9c3a3f
Olle Ronstad <olle.ronstad@gmail.com>
2022-01-24 09:07:34 +0100

    add functinalty to display text

--- basemenu.rb
+++ basemenu.rb
@@ -10,7 +10,7 @@ class BaseMenu
         @leftArrow = Gosu::Image.from_text("<",LEVELFONTSIZE, {bold: true, font: "impact"})
         @rightArrow = Gosu::Image.from_text(">",LEVELFONTSIZE, {bold: true, font: "impact"})
         @copyright = Gosu::Image.from_text("© Ronstad, Olle & Söderborg, Viktor",LEVELFONTSIZE, {bold: true, font: "impact"})
-        
+        @extratextImg =  Gosu::Image.from_text("",LEVELFONTSIZE, {bold: true, font: "impact"})
         @menuid = nil
         @last_x = 0
         @last_y = 0
@@ -19,8 +19,9 @@ class BaseMenu
 
     def draw
         @maintext.draw((@window.width-@maintext.width)*0.5,LEVELFONTSIZE+LEVELFONTTEXTSPACING+TOPSPACING)
+        @extratextImg.draw((@window.width-@extratextImg.width)*0.5,MAINFONTSIZE*MAINFONTSPACING+TOPSPACING-20)
         @textItemsImgs.each_with_index do |text, i|
-            text.draw((@window.width-text.width)*0.5,i*(LEVELFONTSIZE+LEVELFONTTEXTSPACING)+MAINFONTSIZE*MAINFONTSPACING+TOPSPACING)
+            text.draw((@window.width-text.width)*0.5,i*(LEVELFONTSIZE+LEVELFONTTEXTSPACING)+MAINFONTSIZE*MAINFONTSPACING+TOPSPACING+@extratextImg.height)
         end
         if movedmouse?
             hoveringID = idHoveringOver()
@@ -29,8 +30,8 @@ class BaseMenu
             hoveringID = @menuid
         end
         if !hoveringID.nil?
-            @leftArrow.draw((@window.width+@textItemsImgs[hoveringID].width)*0.5,hoveringID*(LEVELFONTSIZE+LEVELFONTTEXTSPACING)+MAINFONTSIZE*MAINFONTSPACING+TOPSPACING)
-            @rightArrow.draw((@window.width-@textItemsImgs[hoveringID].width)*0.5-@rightArrow.width,hoveringID*(LEVELFONTSIZE+LEVELFONTTEXTSPACING)+MAINFONTSIZE*MAINFONTSPACING+TOPSPACING)
+            @leftArrow.draw((@window.width+@textItemsImgs[hoveringID].width)*0.5,hoveringID*(LEVELFONTSIZE+LEVELFONTTEXTSPACING)+MAINFONTSIZE*MAINFONTSPACING+TOPSPACING+@extratextImg.height)
+            @rightArrow.draw((@window.width-@textItemsImgs[hoveringID].width)*0.5-@rightArrow.width,hoveringID*(LEVELFONTSIZE+LEVELFONTTEXTSPACING)+MAINFONTSIZE*MAINFONTSPACING+TOPSPACING+@extratextImg.height)
         end
         @copyright.draw((@window.width-@copyright.width)*0.5,@window.height-50)
     end
@@ -92,7 +93,7 @@ class BaseMenu
 
     def idHoveringOver
         if @window.mouse_x > @window.width/4 && @window.mouse_x < @window.width/4*3
-            tmp = ((@window.mouse_y-MAINFONTSIZE*MAINFONTSPACING-TOPSPACING)/(LEVELFONTSIZE+LEVELFONTTEXTSPACING)).floor
+            tmp = ((@window.mouse_y-MAINFONTSIZE*MAINFONTSPACING-TOPSPACING-@extratextImg.height)/(LEVELFONTSIZE+LEVELFONTTEXTSPACING)).floor
             if tmp < 0 || tmp > @textItems.length-1
                 return nil
             else
