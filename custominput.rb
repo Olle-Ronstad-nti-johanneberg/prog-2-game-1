414d1fe31eb831850e06f30badf0f0c2df0acd27
Olle Ronstad <olle.ronstad@gmail.com>
2022-02-15 20:15:41 +0100

    add custom input handler

---
+++ custominput.rb
@@ -0,0 +1,22 @@
+require 'gosu'
+
+
+def up?()
+    Gosu.button_down?(Gosu::KB_DOWN)||Gosu.button_down?(Gosu::KB_S)
+end
+
+def down?()
+    Gosu.button_down?(Gosu::KB_UP)||Gosu.button_down?(Gosu::KB_W)
+end
+
+def right?()
+    Gosu.button_down?(Gosu::KB_RIGHT)||Gosu.button_down?(Gosu::KB_D)
+end
+
+def left?()
+    Gosu.button_down?(Gosu::KB_A)||Gosu.button_down?(Gosu::KB_LEFT)
+end
+
+def enter?()
+    (button_down?(Gosu::MS_LEFT))||button_down?(Gosu::KB_RETURN)
+end
